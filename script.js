cf3583fe32ce3a2e43cad77e029a6b49344ecd5e
shalinekoh <kohshaline@gmail.com>
2024-06-21 17:12:31 +0800

    Add gameboard, game and render() logic

--- script.js
+++ script.js
@@ -1,4 +1,47 @@
 startButton = document.querySelector("form button")
-startButton.addEventListener("click", () =>{
+player1Name = document.getElementById("player1")
+player2Name = document.getElementById("player2")
+gameBoard = document.querySelector(".game-board")
+
+startButton.addEventListener("click", (event) =>{
+    event.preventDefault();
     Game.start();
 })
+
+const createPlayers = (name, mark) => {
+    return {
+        name,
+        mark
+    }
+}
+
+const Gameboard = (() => {
+    board = ['X','O','','','','','','',''];
+
+    const render = () => {
+        board.forEach((element, index) => {
+            cell = document.createElement("div");
+            cell.setAttribute("id", "cell")
+            cell.innerHTML = element;
+            gameBoard.appendChild(cell)
+        });
+    }
+    return {
+        board,
+        render,
+    }
+})();
+
+const Game = (() => {
+    const start = () => {
+        console.log("Started!")
+        player1 = createPlayers(player1Name.value, "X")
+        player2 = createPlayers(player2Name.value, "O")
+        Gameboard.render();
+    }
+    return {
+        start
+    }
+})();
+
+// Gameboard.render();
